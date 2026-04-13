90e49f6f1de937f4ae6b1a3d7113f785809ed2e6
Gavin Morrow <gavinfmorrow@gmail.com>
2026-04-13 19:53:53 -0400

    Show supervisor/sasl reports

--- server/src/subway_gleam/server/logger_config_ffi.erl
+++ server/src/subway_gleam/server/logger_config_ffi.erl
@@ -4,6 +4,12 @@
 
 % TODO: figure out how to pass in level or smth
 configure_level_info() ->
-    logger:update_primary_config(#{level => info}).
+    logger:update_primary_config(#{
+        level => info,
+        filters => [{progress, {fun logger_filters:progress/2, stop}}]
+    }).
 configure_level_all() ->
-    logger:update_primary_config(#{level => all}).
+    logger:update_primary_config(#{
+        level => all,
+        filters => [{progress, {fun logger_filters:progress/2, stop}}]
+    }).
